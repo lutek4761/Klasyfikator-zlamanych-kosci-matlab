73f23459d73afaf6d4cd7ecd5b7ae002e20ffc93
lutek4671 <nightmarexiqq@gmail.com>
2020-12-07 16:40:41 +0100

    Create main.m

---
+++ main.m
@@ -0,0 +1,48 @@
+clc, clear, close all
+
+digitDatasetPath = fullfile(matlabroot,'toolbox','nnet','nndemos', ...
+    'nndatasets','DigitDataset');
+imds = imageDatastore(digitDatasetPath, ...
+    'IncludeSubfolders',true,'LabelSource','foldernames');
+
+%Pokazuje 20 losowych cyfr 
+figure;
+perm = randperm(10000,20);
+for i = 1:20
+    subplot(4,5,i);
+    imshow(imds.Files{perm(i)});
+end
+
+%Obliczenie liczby kategorii
+labelCount = countEachLabel(imds);
+
+%Okreslenie rozmiaru obrazka na potrzeby warstwy wejsciowej
+img = readimage(imds,1);
+size(img);
+
+%Podzielenie zestawu na dane uczace i testowe
+numTrainFiles = 750;
+[imdsTrain,imdsValidation] = splitEachLabel(imds,numTrainFiles,'randomize');
+
+layers = [
+    imageInputLayer([28 28 1])
+    
+    convolution2dLayer(3,8,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    maxPooling2dLayer(2,'Stride',2)
+    
+    convolution2dLayer(3,16,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    maxPooling2dLayer(2,'Stride',2)
+    
+    convolution2dLayer(3,32,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    fullyConnectedLayer(10)
+    softmaxLayer
+    classificationLayer];
